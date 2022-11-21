9dccde50bf7ef5a0d3d1dcf451c5411ec106a7a2
Dark-Light-20 <pazsebas@hotmail.com>
2022-11-21 17:22:11 -0500

    Timer change with colors (didupdatewidget implementation)

--- proyecto_flutter/didupdatewidget/lib/pages/timer/timer_page.dart
+++ proyecto_flutter/didupdatewidget/lib/pages/timer/timer_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -54,6 +56,33 @@ class TimerView extends StatefulWidget {
 
 class _TimerViewState extends State<TimerView> {
   int _time = 0;
+  late Timer _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(
+      const Duration(seconds: 1),
+      (_) {
+        _time++;
+        setState(() {});
+      },
+    );
+  }
+
+  @override
+  void didUpdateWidget(covariant TimerView oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    print("old: ${oldWidget.color}");
+    print("current: ${widget.color}");
+
+    if (oldWidget.color != widget.color) {
+      print("The colors are different");
+      _time = 0;
+    } else {
+      print("The colors are the same");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {

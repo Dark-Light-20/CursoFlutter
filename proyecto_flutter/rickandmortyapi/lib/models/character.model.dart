ed39f98f0d8003cf2c29d1322bf94dc7e1724376
Dark-Light-20 <pazsebas@hotmail.com>
2022-11-22 18:39:01 -0500

    Character models

---
+++ proyecto_flutter/rickandmortyapi/lib/models/character.model.dart
@@ -0,0 +1,65 @@
+import 'dart:convert';
+
+class Character {
+  Character({
+    required this.id,
+    required this.name,
+    required this.status,
+    required this.species,
+    this.type,
+    required this.gender,
+    required this.origin,
+    required this.location,
+    required this.image,
+    required this.episode,
+    required this.url,
+    required this.created,
+  });
+
+  int id;
+  String name;
+  String status;
+  String species;
+  String? type;
+  String gender;
+  Location origin;
+  Location location;
+  String image;
+  List<String> episode;
+  String url;
+  DateTime created;
+
+  factory Character.fromJson(String str) => Character.fromMap(json.decode(str));
+
+  factory Character.fromMap(Map<String, dynamic> json) => Character(
+        id: json["id"],
+        name: json["name"],
+        status: json["status"],
+        species: json["species"],
+        type: json["type"],
+        gender: json["gender"],
+        origin: Location.fromMap(json["origin"]),
+        location: Location.fromMap(json["location"]),
+        image: json["image"],
+        episode: List<String>.from(json["episode"].map((x) => x)),
+        url: json["url"],
+        created: DateTime.parse(json["created"]),
+      );
+}
+
+class Location {
+  Location({
+    required this.name,
+    required this.url,
+  });
+
+  String name;
+  String url;
+
+  factory Location.fromJson(String str) => Location.fromMap(json.decode(str));
+
+  factory Location.fromMap(Map<String, dynamic> json) => Location(
+        name: json["name"],
+        url: json["url"],
+      );
+}
---
+++ proyecto_flutter/rickandmortyapi/lib/models/characters_response.model.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:rickandmortyapi/models/character.model.dart';
+import 'package:rickandmortyapi/models/info.model.dart';
+
+class CharactersResponse {
+  CharactersResponse({
+    required this.info,
+    required this.characters,
+  });
+
+  Info info;
+  List<Character> characters;
+
+  factory CharactersResponse.fromJson(String str) =>
+      CharactersResponse.fromMap(json.decode(str));
+
+  factory CharactersResponse.fromMap(Map<String, dynamic> json) =>
+      CharactersResponse(
+        info: Info.fromMap(json["info"]),
+        characters: List<Character>.from(
+            json["results"].map((x) => Character.fromMap(x))),
+      );
+}
